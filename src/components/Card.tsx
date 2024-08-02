0d4fa9bfb1f52b37d4813166fdabeb4b6fc4eb70
Muhammad Raffey <muhammadraffey26@gmail.com>
2024-08-02 13:34:12 +0500

    mobile view updated

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,6 +13,8 @@ import { toast } from "react-hot-toast";
 import Link from "next/link";
 import { useCart } from "../context/CartContext";
 import { Image as iImg } from "sanity";
+import { Plus } from "lucide-react";
+import { BiDetail } from "react-icons/bi";
 
 interface ProductType {
   title: string;
@@ -41,14 +43,14 @@ const CardComponent: React.FC<CardComponentProps> = ({ products }) => {
   };
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-5 px-7 lg:px-40 py-16">
+    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3  gap-5 px-4 lg:px-40 py-16">
       {products.map((item) => (
         <Card
           key={item._id}
-          className="flex border-zinc-50/60 border-2 transition-all duration-100 hover:drop-shadow-primary drop-shadow-md hover:shadow-primary justify-center flex-col items-center   gap-3 cursor-pointer"
+          className="flex border-zinc-50/60 border-2 transition-all duration-100 hover:drop-shadow-primary drop-shadow-md hover:shadow-primary justify-center flex-col items-center   sm:gap-3 cursor-pointer"
         >
           <CardHeader>
-            <CardTitle className="font-bold font-sans normal-case text-2xl sm:text-4xl inline-block truncate overflow-hidden whitespace-nowrap">
+            <CardTitle className="font-bold font-sans normal-case text-xl sm:text-4xl inline-block truncate overflow-hidden whitespace-nowrap">
               {item.title}
             </CardTitle>
           </CardHeader>
@@ -70,22 +72,22 @@ const CardComponent: React.FC<CardComponentProps> = ({ products }) => {
                 className="rounded-lg hover:scale-[1.02] hover:shadow-lg duration-300 transition-all hover:shadow-primary aspect-square"
               />
             )}
-            <p className="text-center text-pretty font-bold text-4xl -mb-6 mt-4">
+            <p className="text-center text-pretty font-bold text-xl sm:text-4xl -mb-6 mt-4">
               ${item.price}
             </p>
           </CardContent>
           <CardFooter>
             <p>{item.category.name}</p>
             <Link href={`/products/${item._id}`}>
-              <button className="btn transition-all duration-200 hover:scale-105 hover: font-bold btn-outline btn-md rounded-lg mr-2">
-                Details
+              <button className="btn transition-all duration-200 hover:scale-105 hover:font-bold btn-outline btn-md rounded-lg mr-2 ml-2">
+                <BiDetail className="w-6 h-6" />
               </button>
             </Link>
             <button
-              className="btn transition-all duration-200 hover:scale-105 hover: btn-outline btn-md rounded-lg mr-2 ml-10 hover:font-bold"
+              className="btn transition-all duration-200 hover:scale-105 hover: btn-outline btn-md rounded-lg mr-2 ml-0 sm:ml-10 hover:font-bold "
               onClick={() => handleAddToCart(item._id)}
             >
-              Add to Cart
+              <Plus />
             </button>
           </CardFooter>
         </Card>
